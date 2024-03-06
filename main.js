6f5d95f380a30e7a6cb69021d3dda8f24c10f851
NakaylaRSHart <nakaylahart@DESKTOP-SJTLOE9>
2024-03-06 15:01:55 -0700

    Wrote out psuedo code

---
+++ main.js
@@ -0,0 +1,61 @@
+/*
+Create a calculator that;
+-adds sums
+-subtracts
+-divides
+-multiplies
+-Returns negative and positive numbers
+-converts numbers to percents
+
+-can get random integers
+-get pie 3.14
+-get remainders
+-gets squareroots
+-Doesn't show -0
+-Returns a helpful message instead of NaN if there is an error.
+*/
+//-----------------------------------------------------------------
+
+
+//Create Function called `getSum`
+//  Have it add two numbers
+//  Returns answer
+
+//Create Funtion called `getDifference`
+//  Have it subtract two numbers
+//  Returns answer
+
+//Create Function called `getProduct`
+//  Have it multiply two numbers
+//  Returns answer
+
+//Create Function called `getDividend`
+//  Have it divide two numbers
+//  Returns answer
+
+//Create Function called `getInverse` that allows positive and negative numbers
+
+//Create Function `convertedDecimaltoPercentage`
+//  Collect decimal into function
+//  Math the decimal to make percentage
+//  Return answer
+
+//Create Function `getRandomInteger`
+//  Use math.random function to get random number
+//  Make that number a whole number either with round or .floor/.ceil
+//  Return answer
+
+//Create Function `getPi`
+//  Make it return 3.14
+//  Maybe by user asking for pi?
+
+//Create Function `getRemainder`
+//  Gets the remainder if two numbers are divided
+//  Returns answer
+
+//Create Function `getSquareRoot`
+//  Returns square root of a number
+
+//Create Function `helpfulMessage`
+//  When getting NaN or Undefined return a helpful message.
+
